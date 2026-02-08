8ec15e923e843ba959da64bd29ea9784bca0d73a
ashiq1919 <sallyfox270@gmail.com>
2026-02-08 06:24:01 +0000

    added annotations to entity classes

--- MoneyTransferSystem/src/main/java/com/fidelity/mts/domain/model/Account.java
+++ MoneyTransferSystem/src/main/java/com/fidelity/mts/domain/model/Account.java
@@ -1,15 +1,45 @@
 package com.fidelity.mts.domain.model;
-import java.sql.Timestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import com.fidelity.mts.domain.enums.Enums.AccountStatus;
+import com.fidelity.mts.domain.exception.AccountNotActiveException;
+import com.fidelity.mts.domain.exception.InsufficientBalanceException;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+
+@Entity
+//@Table(name = "accounts")
 public class Account {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@NotNull
+	@Column(name = "holder_name")
 	private String holderName;
-	private long balance;
+	
+	@NotNull
+	@Column(name = "balance", precision = 18)
+	private BigDecimal balance;
+	
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	@Column(name = "status", length = 20)
 	private AccountStatus status;
-	private int version;
-	private Timestamp lastUpdated;
-	public Account(int id, String holderName, long balance, AccountStatus status, int version, Timestamp lastUpdated) {
+	
+	@Version
+	@Column(name = "version")
+	private int version = 0;
+	
+	@Column(name = "last_updated")
+	private LocalDateTime lastUpdated;
+
+	public Account(long id, @NotNull String holderName, @NotNull BigDecimal balance, @NotNull AccountStatus status,
+			int version, LocalDateTime lastUpdated) {
 		super();
 		this.id = id;
 		this.holderName = holderName;
@@ -18,6 +48,34 @@ public class Account {
 		this.version = version;
 		this.lastUpdated = lastUpdated;
 	}
+	
+	public boolean isActive() {
+		return getStatus().equals(AccountStatus.ACTIVE);
+	}
+	
+	public void debit(BigDecimal amount) {
+		if (!this.isActive()) {
+			throw new AccountNotActiveException(this.id);
+		}
+		
+		if (this.balance.compareTo(amount) < 0) {
+			throw new InsufficientBalanceException(this.id, amount, balance);
+		}
+		
+		this.balance = this.balance.subtract(amount);
+		this.lastUpdated = LocalDateTime.now();
+	}
+	
+	public void credit(BigDecimal amount) {
+		if (!this.isActive()) {
+			throw new AccountNotActiveException(this.id);
+		}
+		
+		this.balance = this.balance.add(amount);
+		this.lastUpdated = LocalDateTime.now();
+				
+	}
+	
 	public long getId() {
 		return id;
 	}
@@ -30,10 +88,10 @@ public class Account {
 	public void setHolderName(String holderName) {
 		this.holderName = holderName;
 	}
-	public long getBalance() {
+	public BigDecimal getBalance() {
 		return balance;
 	}
-	public void setBalance(long balance) {
+	public void setBalance(BigDecimal balance) {
 		this.balance = balance;
 	}
 	public AccountStatus getStatus() {
@@ -48,14 +106,12 @@ public class Account {
 	public void setVersion(int version) {
 		this.version = version;
 	}
-	public Timestamp getLastUpdated() {
+	public LocalDateTime getLastUpdated() {
 		return lastUpdated;
 	}
-	public void setLastUpdated(Timestamp lastUpdated) {
+	public void setLastUpdated(LocalDateTime lastUpdated) {
 		this.lastUpdated = lastUpdated;
 	}
-	
-	
-	
+
 	
 }
--- MoneyTransferSystem/src/main/java/com/fidelity/mts/domain/model/TransactionLog.java
+++ MoneyTransferSystem/src/main/java/com/fidelity/mts/domain/model/TransactionLog.java
@@ -2,30 +2,59 @@ package com.fidelity.mts.domain.model;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.UUID;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
 import com.fidelity.mts.domain.enums.Enums.TransactionStatus;
 
+@Entity
+@Table(name = "transaction_logs")
 public class TransactionLog {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(length = 36)
 	private UUID id;
+	
+	@NotNull
+	@Column(name = "from_account")
 	private long fromAccountId;
+	
+	@NotNull
+	@Column(name = "to_account")
 	private long toAccountId;
+	
+	@NotNull
+	@Column(name = "amount", precision = 18)
 	private BigDecimal amount;
+	
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	@Column(name = "status", length = 20)
 	private TransactionStatus status;
+	
+	@Column(name = "failure_reason")
 	private String failureReason;
+	
+	@Column(name = "idempotency_key", unique = true, length = 100)
 	private String idempotencyKey;
-	private Timestamp createdOn;
-	public TransactionLog(UUID id, long fromAccountId, long toAccountId, BigDecimal amount, TransactionStatus status,
-			String failureReason, String idempotencyKey, Timestamp createdOn) {
-		super();
-		this.id = id;
+	
+	@Column(name = "created_on")
+	private LocalDateTime createdOn;
+	
+	public TransactionLog() {}
+	public TransactionLog(long fromAccountId, long toAccountId, BigDecimal amount, TransactionStatus status,
+			String failureReason, String idempotencyKey) {
+		this.id = UUID.randomUUID();
 		this.fromAccountId = fromAccountId;
 		this.toAccountId = toAccountId;
 		this.amount = amount;
 		this.status = status;
 		this.failureReason = failureReason;
 		this.idempotencyKey = idempotencyKey;
-		this.createdOn = createdOn;
+		this.createdOn = LocalDateTime.now();
 	}
 	public UUID getId() {
 		return id;
@@ -69,10 +98,10 @@ public class TransactionLog {
 	public void setIdempotencyKey(String idempotencyKey) {
 		this.idempotencyKey = idempotencyKey;
 	}
-	public Timestamp getCreatedOn() {
+	public LocalDateTime getCreatedOn() {
 		return createdOn;
 	}
-	public void setCreatedOn(Timestamp createdOn) {
+	public void setCreatedOn(LocalDateTime createdOn) {
 		this.createdOn = createdOn;
 	}
 	
