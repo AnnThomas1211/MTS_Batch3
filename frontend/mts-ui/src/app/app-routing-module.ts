3fe0e3291d9cf2221dec9ea77ef69f7ae84d355b
Ani-ml-droid <126156010@sastra.ac.in>
2026-02-11 08:47:04 +0000

    Update app-routing-module.ts

--- frontend/mts-ui/src/app/app-routing-module.ts
+++ frontend/mts-ui/src/app/app-routing-module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard-component/dashboard-component';
+import { TransferComponent } from './components/transfer-component/transfer-component';
+import { HistoryComponent } from './components/history-component/history-component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : DashboardComponent
+  },
+  {
+    path : 'transfer',
+    component : TransferComponent
+  },
+  {
+    path : 'history',
+    component : HistoryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
