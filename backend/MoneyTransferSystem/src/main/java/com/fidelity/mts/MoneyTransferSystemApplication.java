08f330821247db5260334268c69bd7ef8e1ca895
Kushal Varma Guntimadugu <kushalvarma.guntimadugu@gmail.com>
2026-02-15 13:33:27 +0000

    backend imports for sec

--- backend/MoneyTransferSystem/src/main/java/com/fidelity/mts/MoneyTransferSystemApplication.java
+++ backend/MoneyTransferSystem/src/main/java/com/fidelity/mts/MoneyTransferSystemApplication.java
@@ -1,5 +1,4 @@
 package com.fidelity.mts;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- frontend/mts-ui/src/app/app-module.ts
+++ frontend/mts-ui/src/app/app-module.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './components/login-component/login-component';
 import { DashboardComponent } from './components/dashboard-component/dashboard-component';
 import { HistoryComponent } from './components/history-component/history-component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
 import { ProfileComponent } from './components/profile-component/profile-component';
 
 
@@ -25,11 +25,6 @@ import { ProfileComponent } from './components/profile-component/profile-compone
     ReactiveFormsModule,
   ],
   providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,
-    },
     provideBrowserGlobalErrorListeners(),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
